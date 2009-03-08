2ce548a30fc5e0a0f6a80fe7f18767dbca7cb4e6
Ryan Bates <ryan@railscasts.com>
2009-03-07 20:42:54 -0800

    fixing comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comments])
+    @comment = @post.comments.create!(params[:comment])
     respond_to do |format|
       format.html { redirect_to @post }
       format.js
